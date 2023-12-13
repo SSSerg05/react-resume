b11f742010cbd1eece02e177861d4ed1930cf182
SSSerg05 <ssserg05@gmail.com>
2023-12-14 00:56:41 +0200

    fix
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,13 @@
+import { Aside, Img } from "./SideBar.styled";
+
+
+export const SideBar = () => {
+
+  return (
+    <Aside>
+      <Img href="" alt="avatar"/>
+    </Aside>
+  )
+}
+
+export default SideBar;
---
+++ src/components/SideBar/SideBar.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const Aside = styled.aside`
+  background-color: #1E2939;
+  padding-top: 40px;
+`
+
+export const Img = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
+  outline: 1px solid red;
+`
+
